238a02c6bf1627be646ca977e22e03606a0af3b7
IchiroT <ichir0roie@gmail.com>
2020-12-17 12:53:01 +0900

    add copy

---
+++ getTypingText/getWords.py
@@ -0,0 +1,19 @@
+import os
+
+
+with open("source.txt",mode="r",encoding="utf-8")as f:
+    file=f.readlines()
+
+for i in file:
+    if("<p class=\"kanji\">" in i):
+        i=i.replace("<p class=\"kanji\">","")
+        i=i.replace("</p>","")
+        i=i.replace(" ","").replace("\t","").replace("\n","").replace("\r","")
+        print(i)
+
+    if("<p class=\"kana\">(" in i):
+        i=i.replace("<p class=\"kana\">(","")
+        i=i.replace(")</p>","")
+        i=i.replace(" ","").replace("\t","").replace("\n","").replace("\r","")
+        print(i)
+
