00fb5d171b858f1247c460dc7fcaf7968c9795ad
IchiroT <ichir0roie@gmail.com>
2020-12-19 21:40:40 +0900

    first

---
+++ myTyping/myTyping/Code/TypingGame.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myTyping.Code
+{
+    public class TypingGame
+    {
+        public List<questionData> questions;//todo : finally this is private.
+        public List<questionData> answers;//todo : finally this is private.
+        private System.Diagnostics.Stopwatch watch;
+
+        public int quesPlace = 0;
+
+        public TypingGame()
+        {
+             watch = new System.Diagnostics.Stopwatch();
+            questions = new List<questionData>();
+            answers = new List<questionData>();
+        }
+
+        public void setQuestions(string[] texts)
+        {
+            foreach(var text in texts)
+            {
+                questions.Add(new questionData(text));
+            }
+        }
+
+        public string Ask()
+        {
+            //todo : select question from questionData
+
+            quesPlace = new Random().Next(0,questions.Count);
+
+
+            watch.Start();
+
+            return questions[quesPlace].text;
+        }
+
+
+        public bool response(string input)
+        {
+            bool correct = true;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != questions[quesPlace].text[i])
+                {
+                    correct = false;
+                    questions[quesPlace].missTypes++;
+                    break;
+                }
+            }
+
+            return correct;
+        }
+
+        public bool Entering(string input)
+        {
+            watch.Stop();
+
+            if (input == questions[quesPlace].text)
+            {
+                var data = questions[quesPlace];
+                questions.RemoveAt(quesPlace);
+                quesPlace = -1;
+                data.clear = true;
+                data.time = watch.Elapsed.TotalSeconds;
+
+                answers.Add(data);
+                return true;
+            }
+
+            return false;
+        }
+
+        public questionData getAnswer()
+        {
+            var data = answers[answers.Count - 1];
+
+            return data;
+        }
+
+        public class questionData
+        {
+            public string text = "";
+            public bool clear = false;
+            public double time = 0;
+            public int missTypes = 0;
+
+            public questionData(string _text)
+            {
+                text =_text;
+            }
+        }
+
+
+    }
+}
---
+++ myTyping/myTyping/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace myTyping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.label1.Location = new System.Drawing.Point(6, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(764, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "tlQ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput.Font = new System.Drawing.Font("MS UI Gothic", 20F);
+            this.tbInput.Location = new System.Drawing.Point(6, 307);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(764, 34);
+            this.tbInput.TabIndex = 1;
+            this.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbInput);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 426);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
