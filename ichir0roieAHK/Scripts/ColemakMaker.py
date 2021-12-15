d511e34edbc8d8d0d6bf3e8ce7ac57209e24d3cb
ichir0roieHome <ichir0roie.com>
2021-12-15 23:49:05 +0900

    del

--- ichir0roieAHK/Scripts/ColemakMaker.py
+++
@@ -1,9 +0,0 @@
-
-
-qwrtext = "qwertyuiopasdfghjklzxcvbnm"
-colemak = "qwfpgjluyharstdneiozxcvbkm"
-
-if __name__ == '__main__':
-    for c in range(len(qwrtext)):
-        if qwrtext[c] != colemak[c]:
-            print(qwrtext[c] + "::"+colemak[c])
